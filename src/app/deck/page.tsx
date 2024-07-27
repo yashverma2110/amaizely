1454b05b9f15362b114238cbb31fdef8a11a0983
Yash Verma <yash.silver2110@gmail.com>
2024-07-27 13:52:38 +0530

    fix: decks not loading on page

--- src/app/deck/page.tsx
+++ src/app/deck/page.tsx
@@ -8,15 +8,15 @@ import { cookies } from "next/headers";
 import { setHeaders } from "@/config/AxiosService";
 
 export default async function DeckPage() {
-  const deckResponse = await GET_MY_DECKS();
   const cookieStore = cookies();
-
   const authSid = cookieStore.get('sid')?.value;
   if (authSid) {
     setHeaders({
       Cookie: `sid=${authSid}`
     })
   }
+
+  const deckResponse = await GET_MY_DECKS();
 
   const getDeckBgColor = (index: number) => {
     const colors = ['bg-red-300', 'bg-blue-300', 'bg-green-300', 'bg-yellow-300', 'bg-indigo-300', 'bg-purple-300', 'bg-pink-300'];
