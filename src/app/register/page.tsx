e9c6112e2a4e78e0c553774a339744f05dd677b6
Yash Verma <yash.silver2110@gmail.com>
2024-09-01 12:47:23 +0530

    fix: added suspense on auth form

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,11 +1,14 @@
 import { AuthForm } from "@/components/AuthForm";
+import { Suspense } from "react";
 
 export default function Register() {
   return (
     <main className="register-page h-screen flex justify-center items-center p-4">
       <div className="mockup-window shadow-lg border border-gray-400">
         <div className="flex p-4 border-t border-gray-400">
-          <AuthForm className="w-full" type="register" />
+          <Suspense fallback={<div className="skeleton w-full h-96"></div>}>
+            <AuthForm className="w-full" type="register" />
+          </Suspense>
         </div>
       </div>
     </main>
