9af2a88efff80d383a94e1d176a4b30145154d13
Yash Verma <yash.silver2110@gmail.com>
2024-11-01 19:22:53 +0530

    fix: making header and bottom nav sticky

--- src/layouts/InAppLayout.tsx
+++ src/layouts/InAppLayout.tsx
@@ -11,7 +11,7 @@ export default function InAppLayout({
     <div className="in-app-layout bg-gray-200 h-screen grid grid-cols-12 grid-rows-12">
       <Sidebar className="col-span-3 row-span-12 hidden md:flex md:flex-col" />
       <div className="blob-bg h-1/2 w-full md:w-3/4 !z-0 md:scale-150 fixed -bottom-[20%] -right-[25%]" />
-      <AppHeader className="row-span-1 col-span-12 md:hidden" />
+      <AppHeader className="row-span-1 col-span-12 md:hidden sticky top-0" />
 
       <section className="content-container col-span-12 row-span-10 md:row-span-12 grid grid-rows-12 md:col-span-9">
         <div className="row-span-12 z-10 overflow-y-auto shadow-inner">
@@ -19,7 +19,7 @@ export default function InAppLayout({
         </div>
       </section>
 
-      <BottomNavigation className="row-span-1 col-span-12 z-10 md:hidden" />
+      <BottomNavigation className="row-span-1 col-span-12 z-10 md:hidden sticky bottom-0" />
     </div>
   );
 }
