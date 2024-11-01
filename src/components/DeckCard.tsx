f06d8adf3231429ab296d8a50410a7c4b7fd7041
Yash Verma <yash.silver2110@gmail.com>
2024-11-01 20:24:56 +0530

    fix: asset overflow deck card

--- src/components/DeckCard.tsx
+++ src/components/DeckCard.tsx
@@ -8,9 +8,9 @@ export default function DeckCard({ bgClass, deck }: { deck: IDeck, bgClass?: str
   return (
     <div className="deck-card">
       <div className="stack w-full">
-        <div className={`relative overflow-hidden border-base-content h-full card border ${bgClass ?? 'bg-base-100'}`}>
-          <div className="circle absolute -top-2 -right-4 skew-x-6 w-32 h-10 rounded-full bg-yellow-200" />
-          <div className="card-body p-4">
+        <div className={`border-base-content h-full card border ${bgClass ?? 'bg-base-100'}`}>
+          <div className="card-body relative overflow-hidden p-4">
+            <div className="circle absolute -top-2 -right-4 skew-x-6 w-32 h-10 rounded-full bg-yellow-200" />
             <div className="card-title flex justify-between">
               <h2 className=" text-xl font-bold">{deck.title}</h2>
               <DeckDeletion deckId={deck._id} deckName={deck.title} />
