98c41d1f26d7815850b02b2ff81219f706de2b8e
Yash Verma <yash.silver2110@gmail.com>
2024-11-02 15:34:28 +0530

    fix: deck editor loading

--- src/app/deck/edit/[deckId]/loading.tsx
+++ src/app/deck/edit/[deckId]/loading.tsx
@@ -1,4 +1,4 @@
-export default function DeckLoading() {
+export default function DeckEditLoading() {
   return (
     <section className="deck-loading w-full flex flex-col items-center p-4 gap-4">
       <div className="skeleton bg-gray-300 h-12 rounded w-full drop-shadow"></div>
--- src/app/deck/edit/[deckId]/page.tsx
+++ src/app/deck/edit/[deckId]/page.tsx
@@ -8,7 +8,7 @@ import { IDeck } from "@/types/IDeck";
 export default function EditDeckPage({ params }: { params: { deckId: string } }) {
   const [deck, setDeck] = useState<IDeck>();
   const [flashcards, setFlashcards] = useState<IFlashcard[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     GET_DECK_AND_DECK_CARDS_WITH_ID(params.deckId).then((response) => {
@@ -20,6 +20,17 @@ export default function EditDeckPage({ params }: { params: { deckId: string } })
       setIsLoading(false);
     });
   }, [params.deckId])
+
+  if (isLoading) {
+    return (
+      <section className="deck-loading w-full flex flex-col items-center p-4 gap-4">
+        <div className="skeleton bg-gray-300 h-12 rounded w-full drop-shadow"></div>
+        <div className="skeleton bg-gray-300 h-12 rounded w-full drop-shadow"></div>
+        <div className="skeleton bg-gray-300 h-52 w-full drop-shadow"></div>
+        <div className="skeleton bg-gray-300 h-52 w-full drop-shadow"></div>
+      </section>
+    )
+  }
 
   return (
     <DeckEditor mode="edit" deck={deck} flashcards={flashcards} />
