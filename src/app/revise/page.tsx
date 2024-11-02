aa3ec7b8350512885d1c5b98cb79d9ac9f030161
Yash Verma <yash.silver2110@gmail.com>
2024-11-02 16:17:35 +0530

    feat: added empty deck revision placeholder

--- src/app/revise/page.tsx
+++ src/app/revise/page.tsx
@@ -1,7 +1,10 @@
 import FlashcardContainer from "@/components/FlashcardContainer";
 import { setHeaders } from "@/config/AxiosService";
 import { GET_FLASHCARDS_FOR_REVISION } from "@/services/FlashcardService";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { cookies } from "next/headers";
+import Link from "next/link";
 
 export default async function RevisePage() {
   const cookieStore = cookies();
@@ -16,6 +19,25 @@ export default async function RevisePage() {
 
   if (!response.success) {
     return <div>Failed to load flashcards</div>
+  }
+
+  if (response.flashcards.length === 0) {
+    return (
+      <section className="empty-placeholder h-full p-4 flex items-center">
+        <div className="card bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">No flashcards found</h2>
+            <p>Create a deck to start with your smart revisions</p>
+            <div className="card-actions">
+              <Link href="/deck?mode=create" className="btn btn-primary w-full">
+                <FontAwesomeIcon icon={faPlus} />
+                Create a deck
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
   }
 
   return (
