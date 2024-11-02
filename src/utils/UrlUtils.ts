a5c4d2f6a580fe91b28aaeac483860aacc35854c
Yash Verma <yash.silver2110@gmail.com>
2024-11-02 14:17:47 +0530

    fix: url validation

--- src/utils/UrlUtils.ts
+++ src/utils/UrlUtils.ts
@@ -18,13 +18,8 @@ export function isValidYouTubeUrl(url: string) {
 export function isValidWebsiteURL(url: string): boolean {
   // Regular expression for URL validation
   const urlPattern = new RegExp(
-    '^(https?:\\/\\/)?' + // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-    '(\\#[-a-z\\d_]*)?$', // fragment locator
-    'i' // case-insensitive
+    'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)',
+    'i'
   );
 
   return urlPattern.test(url);
