1ec8929da536efdc44d09bfc97fd85976452db2a
Yash Verma <yash.silver2110@gmail.com>
2024-11-02 23:58:35 +0530

    revert: added get deck by id

--- src/services/DeckService.ts
+++ src/services/DeckService.ts
@@ -294,6 +294,32 @@ export async function UPDATE_DECK_AND_DECK_CARDS_BY_ID(deckId: string, deck: any
   }
 }
 
+export async function GET_DECK_AND_DECK_CARDS_WITH_ID(deckId: string) {
+  try {
+    const response = await AxiosInstance.get(`/deck/${deckId}`)
+
+    return {
+      success: true,
+      deck: response.data.deck,
+      flashcards: response.data.flashcards
+    }
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      return {
+        success: false,
+        status: error.response?.status,
+        message: error.response?.data.message
+      }
+    }
+
+    return {
+      success: false,
+      error,
+    }
+  }
+}
+
+
 export async function DELETE_DECK_WITH_ID(deckId: string) {
   try {
     const response = await AxiosInstance.delete(`/deck/delete/${deckId}`)
