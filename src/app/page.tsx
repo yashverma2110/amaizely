f582fbe7189c50b7ad138e0df34fde9e4d479051
Yash Verma <yash.silver2110@gmail.com>
2024-11-08 20:54:42 +0530

    feat: landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,11 @@ export default function Home() {
         <div className="blob-bg h-1/2 w-full md:w-3/4 md:scale-150 fixed -top-[20%] -right-[25%]" />
         <div className="hero-content text-center">
           <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Amaizely</h1>
-            <p className="mb-5">
+            <h1 className="mb-5 text-5xl md:text-6xl font-bold">Amaizely</h1>
+            <p className="mb-5 md:text-xl">
               Don&apos;t just read, remember! A smart way to create flashcards to help you stay ahead.
             </p>
-            <button className="btn btn-primary">
+            <button className="btn md:btn-lg btn-primary">
               <Link href="/login">
                 Get Started
               </Link>
@@ -22,42 +22,53 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="fold-2 min-h-screen bg-white bg-opacity-80 p-4 gap-4 flex items-center flex-col">
-        <div className="flex items-center gap-2">
-          <label className="input input-bordered flex items-center gap-2">
-            Topic
-            <input type="text" className="grow pointer-events-none" placeholder="What can Amaizely do?" />
-          </label>
-          <button className="btn btn-primary">
-            Generate
-          </button>
-        </div>
-
-        <div className="card bg-base-100 shadow-xl">
+      <section className="fold-2 min-h-screen bg-purple-300 bg-opacity-30 p-4 gap-4 grid md:grid-cols-3 grid-cols-1 md:grid-rows-12">
+        <div className="card bg-base-100 shadow-xl md:row-span-4">
           <div className="card-body">
-            <h2 className="card-title">Generate flashcards using AI</h2>
-            <p>Generate flashcards from the content of <strong>Websites</strong>, <strong>YouTube</strong>, <strong>books</strong>, <strong>PDFs</strong>, and more!</p>
+            <h2 className="card-title">Generate using AI</h2>
+            <p className="text-base md:text-lg">Generate flashcards from the content of <strong>Websites</strong>, <strong>YouTube</strong>, <strong>books</strong>, <strong>PDFs</strong>, and more!</p>
           </div>
         </div>
 
-        <div className="card bg-base-100 shadow-xl">
+        <div className="card bg-base-100 shadow-xl md:row-span-6">
           <div className="card-body">
             <h2 className="card-title">Smart revisions</h2>
-            <p>Amaizely keeps track of the content you need to revise and helps you revise it using smart spaced repetition.</p>
+            <p className="text-base md:text-lg">Amaizely keeps track of the content you need to revise and helps you revise it using smart spaced repetition.</p>
           </div>
         </div>
 
-        <div className="card bg-base-100 shadow-xl">
+        <div className="card bg-base-100 shadow-xl md:row-span-4">
+          <div className="card-body">
+            <h2 className="card-title">Revise specific topics</h2>
+            <p className="text-base md:text-lg">Amaizely allows you to revise decks of specific topics or subjects that you want to focus on.</p>
+          </div>
+        </div>
+
+        <div className="card bg-base-100 shadow-xl md:row-span-7">
+          <div className="card-body">
+            <h2 className="card-title">Formatting tools</h2>
+            <p className="text-base md:text-lg">Amaizely has a set of tools to help you format your flashcards and make them look beautiful.</p>
+          </div>
+        </div>
+
+        <div className="card bg-base-100 shadow-xl md:row-span-3">
           <div className="card-body">
             <h2 className="card-title">Choose your decks</h2>
-            <p>Save other&apos;s decks or create your own and choose which one you want to revise.</p>
+            <p className="text-base md:text-lg">Save other&apos;s decks or create your own and choose which one you want to revise.</p>
           </div>
         </div>
 
-        <div className="card bg-base-100 shadow-xl">
+        <div className="card bg-base-100 shadow-xl md:row-span-4">
           <div className="card-body">
             <h2 className="card-title">Share your decks</h2>
-            <p>Share your decks with your friends and family to collaborate and revise together!</p>
+            <p className="text-base md:text-lg">Share your decks with your friends and family to revise together!</p>
+          </div>
+        </div>
+
+        <div className="card bg-base-100 shadow-xl md:row-span-4 border-b-2 border-warning">
+          <div className="card-body">
+            <h2 className="card-title">Pay as you go</h2>
+            <p className="text-base md:text-lg">Amaizely let&apos;s you buy decks as you go, escape the headache of managing multiple subscriptions.</p>
           </div>
         </div>
       </section>
