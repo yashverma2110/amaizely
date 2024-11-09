eb42480eeeaa98946ac90119c94814bf8a66fc30
Yash Verma <yash.silver2110@gmail.com>
2024-11-09 17:47:51 +0530

    feat: added sitemap generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,36 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://amaizely.com',
+  generateRobotsTxt: true,
+  exclude: ['*'], // Exclude all pages by default
+  generateIndexSitemap: false,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/'
+      }
+    ]
+  },
+  transform: async (config, path) => {
+    const paths = [
+      {
+        loc: '/', // Landing page
+        changefreq: 'weekly',
+        priority: 1.0,
+      },
+      {
+        loc: '/purchase',
+        changefreq: 'weekly',
+        priority: 0.8,
+      },
+      {
+        loc: '/help/flashcard-maker',
+        changefreq: 'monthly',
+        priority: 0.7,
+      }
+    ];
+
+    return paths;
+  }
+}
